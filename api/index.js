9fe75004c042a02e393cfabe224226107c7180a9
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-02 11:14:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,11 +13,18 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-  })
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.error(err);
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
